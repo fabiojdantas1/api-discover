d7a1f18926467cfb1f303065c7ff14ea63c82665
Fábio Dantas <fabiojdantas1@gmail.com>
2022-07-16 08:51:12 -0300

    fix: use eslint in fetch-discover-api.js

--- fetch-discover-api.js
+++ fetch-discover-api.js
@@ -1,79 +1,78 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.listen(5500, () => console.log('Rodando na porta 5500'))
+app.listen(5500, () => console.log('Rodando na porta 5500'));
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
 let users = [{
-  id: 1,
-  name: "Jakeliny Gracielly",
-  avatar: "https://avatars.githubusercontent.com/u/17316392?v=4",
-  city: "São Paulo"
-}]
-
+    id: 1,
+    name: 'Jakeliny Gracielly',
+    avatar: 'https://avatars.githubusercontent.com/u/17316392?v=4',
+    city: 'São Paulo'
+}];
 
 app.route('/api').get((req, res) => res.json({
-  users
-}))
+    users
+}));
 
 app.route('/api/:id').get((req, res) => {
-  const userId = req.params.id
+    const userId = req.params.id;
 
-  const user = users.find(user => Number(user.id) === Number(userId))
+    const user = users.find(user => Number(user.id) === Number(userId));
 
-  if (!user) {
-    return res.json('User nor found!')
-  }
+    if (!user) {
+        return res.json('User nor found!');
+    }
 
-  res.json(user)
-})
+    res.json(user);
+});
 
 app.route('/api').post((req, res) => {
-  const lastId = users[users.length - 1].id
-  users.push({
-    id: lastId + 1,
-    name: req.body.name,
-    avatar: req.body.avatar,
-    city: req.body.city
-  })
-  res.json('Saved user')
-})
+    const lastId = users[users.length - 1].id;
+    users.push({
+        id: lastId + 1,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        city: req.body.city
+    });
+    res.json('Saved user');
+});
 
 app.route('/api/:id').put((req, res) => {
-  const userId = req.params.id
+    const userId = req.params.id;
 
-  const user = users.find(user => Number(user.id) === Number(userId))
+    const user = users.find(user => Number(user.id) === Number(userId));
 
-  if (!user) {
-    return res.json('User nor found!')
-  }
-
-  const updatedUser = {
-    ...user,
-    name: req.body.name,
-    avatar: req.body.avatar,
-    city: req.body.city
-  }
-
-  users = users.map(user => {
-    if (Number(user.id) === Number(userId)) {
-      user = updatedUser
+    if (!user) {
+        return res.json('User nor found!');
     }
-    return user
-  })
 
-  res.json("Updated user")
-})
+    const updatedUser = {
+        ...user,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        city: req.body.city
+    };
+
+    users = users.map(user => {
+        if (Number(user.id) === Number(userId)) {
+            user = updatedUser;
+        }
+        return userId;
+    });
+
+    res.json('Updated user');
+});
 
 app.route('/api/:id').delete((req, res) => {
-  const userId = req.params.id
+    const userId = req.params.id;
 
-  users = users.filter(user => Number(user.id) !== Number(userId))
+    users = users.filter(user => Number(user.id) !== Number(userId));
 
-  res.json('Deleted User')
-})
+    res.json('Deleted User');
+});
